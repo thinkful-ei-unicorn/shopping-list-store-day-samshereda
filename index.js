017db1ee60899603954de73f91934292a9b1a8bd
samshereda <70921492+samshereda@users.noreply.github.com>
2020-11-19 15:56:16 -0500

    Added edit button

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+// eslint-disable-next-line strict
 const store = {
   items: [
-    { id: cuid(), name: 'apples', checked: false },
-    { id: cuid(), name: 'oranges', checked: false },
-    { id: cuid(), name: 'milk', checked: true },
-    { id: cuid(), name: 'bread', checked: false }
+    { id: cuid(), name: 'apples', checked: false, editing: false},
+    { id: cuid(), name: 'oranges', checked: false, editing: false},
+    { id: cuid(), name: 'milk', checked: true, editing: false},
+    { id: cuid(), name: 'bread', checked: false, editing: false}
   ],
   hideCheckedItems: false
 };
@@ -16,6 +17,27 @@ const generateItemElement = function (item) {
     `;
   }
 
+  if (item.editing){
+    return `
+      <li class='js-item-element' data-item-id='${item.id}'>
+        <form class='edit-item-form'>
+        <input type='hidden' class='item-id' name='item-id' value=${item.id}>
+        <input type='text' class='item-title'><br>
+        <div class='shopping-item-controls'>
+          <button class='shopping-item-toggle js-item-toggle'>
+            <span class='button-label'>check</span>
+          </button>
+          <button class='shopping-item-delete js-item-delete'>
+            <span class='button-label'>delete</span>
+          </button>
+          <button type="submit" class='shopping-item-save js-item-save'>
+            <span class='button-label'>save</span>
+          </button>
+        </div>
+        </form>
+      </li>`;
+  }
+
   return `
     <li class='js-item-element' data-item-id='${item.id}'>
       ${itemTitle}
@@ -25,6 +47,9 @@ const generateItemElement = function (item) {
         </button>
         <button class='shopping-item-delete js-item-delete'>
           <span class='button-label'>delete</span>
+        </button>
+        <button class='shopping-item-edit js-item-edit'>
+          <span class='button-label'>edit</span>
         </button>
       </div>
     </li>`;
@@ -43,6 +68,9 @@ const render = function () {
   // variable 'items' that we will reassign to a new
   // version if any filtering of the list occurs.
   let items = [...store.items];
+  for (i of items){
+    console.log(i.name);
+  }
   // If the `hideCheckedItems` property is true, 
   // then we want to reassign filteredItems to a 
   // version where ONLY items with a "checked" 
@@ -127,6 +155,45 @@ const handleDeleteItemClicked = function () {
   });
 };
 
+const handleEditItemClicked = function() {
+  $('.js-shopping-list').on('click', '.js-item-edit', event => {
+    console.log(`edit clicked`);
+    // Get the index of the item in store.items.
+    const id = getItemIdFromElement(event.currentTarget);
+    toggleEditingForListItem(id);
+    // Render the updated shopping list.
+    render();
+  });
+};
+
+const toggleEditingForListItem = function(id) {
+  const foundItem = store.items.find(item => item.id === id);
+  console.log(`${foundItem} editing`);
+  for (i of store.items){
+    i.editing = false;
+  }
+  foundItem.editing = true;
+
+};
+
+const editShoppingListItem = function(id, newTitle){
+  const item =store.items.find(item => item.id ===id);
+  item.name = newTitle;
+  item.editing = false;
+}
+
+const handleEditItemSave = function () {
+  $('.js-shopping-list').on('submit', '.edit-item-form', event => {
+    console.log(`saved`)
+    event.preventDefault();
+    const newTitle = $('.item-title').val();
+    const itemId = $('.item-id').val();
+    console.log(`${newTitle} ${itemId}`)
+    editShoppingListItem(itemId, newTitle);
+    render();
+  });
+};
+
 /**
  * Toggles the store.hideCheckedItems property
  */
@@ -160,6 +227,8 @@ const handleShoppingList = function () {
   handleItemCheckClicked();
   handleDeleteItemClicked();
   handleToggleFilterClick();
+  handleEditItemClicked();
+  handleEditItemSave();
 };
 
 // when the page loads, call `handleShoppingList`
